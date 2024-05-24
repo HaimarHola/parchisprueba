45bb962f5d8da3c65fe101a130fbd7a509fb5dec
DAM101 <DAM101@B0802.iesataulfo.local>
2024-05-24 20:02:58 +0200

    Fase 7- Clase Usuario con atributos

--- parchisClasesPruebasUnits/src/main/java/parchisClasesPruebasUnits/Usuario.java
+++ parchisClasesPruebasUnits/src/main/java/parchisClasesPruebasUnits/Usuario.java
@@ -1,5 +1,13 @@
 package parchisClasesPruebasUnits;
 
 public class Usuario {
-
+	private String username;
+	private String contra;
+	private String color;
+	
+	public Usuario(String username,String contra,String color) {
+		this.username=username;
+		this.contra=contra;
+		this.color=color;
+	}
 }
